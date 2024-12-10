36823225ad1ea1051a5f37815ea08390473f38e4
Tamizh-Mani <manikandanlogu24@gmail.com>
2024-12-10 15:28:04 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,35 @@
-import React, { lazy, Suspense } from 'react';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
-const HomeOne = lazy(() => import('./pages/HomeOne'));
-const About = lazy(() => import('./pages/About'));
-const ServiceUs = lazy(() => import('./pages/Service'));
-const Portfolio = lazy(() => import('./pages/Portfolio'));
-const FAQMain = lazy(() => import('./pages/FAQ'));
-const ContactUs = lazy(() => import('./components/ContactUs/ContactUs'));
-const BlogParent = lazy(() => import('./components/Blog/Blog-parent'));
-const BlogPageOneMain = lazy(()=>import ('./pages/BlogPageOne'))
-const BlogPageTwoMain = lazy(()=>import ('./pages/BlogPageTwo'))
-const BlogPageThreeMain = lazy(()=>import ('./pages/BlogPageThree'))
+import React from 'react';
+import { HashRouter, Route, Routes } from 'react-router-dom';
+
+// Eagerly importing all components
+import HomeOne from './pages/HomeOne';
+import About from './pages/About';
+import ServiceUs from './pages/Service';
+import Portfolio from './pages/Portfolio';
+import FAQMain from './pages/FAQ';
+import ContactUs from './components/ContactUs/ContactUs';
+import BlogParent from './components/Blog/Blog-parent';
+import BlogPageOneMain from './pages/BlogPageOne';
+import BlogPageTwoMain from './pages/BlogPageTwo';
+import BlogPageThreeMain from './pages/BlogPageThree';
+
 const App = () => {
   return (
     <HashRouter>
-      <Router>
-      <Suspense>
-        <Routes>
-          <Route path="/" element={<HomeOne />} />
-          <Route path="/home" element={<HomeOne />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/services" element={<ServiceUs />} />
-          <Route path="/portfolio" element={<Portfolio />} />
-          <Route path="/faq" element={<FAQMain />} />
-          <Route path="/contact" element={<ContactUs />} />
-          <Route path="/blog" element={<BlogParent />} />
-          <Route path="/blog/:id" element={<BlogPageOneMain />} />
-          <Route path="/blog/post2" element={<BlogPageTwoMain />} />
-          <Route path="/blog/post3" element={<BlogPageThreeMain />} />
-        </Routes>
-      </Suspense>
-    </Router>
+      <Routes>
+        <Route path="/" element={<HomeOne />} />
+        <Route path="/home" element={<HomeOne />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<ServiceUs />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/faq" element={<FAQMain />} />
+        <Route path="/contact" element={<ContactUs />} />
+        <Route path="/blog" element={<BlogParent />} />
+        <Route path="/blog/:id" element={<BlogPageOneMain />} />
+        <Route path="/blog/post2" element={<BlogPageTwoMain />} />
+        <Route path="/blog/post3" element={<BlogPageThreeMain />} />
+      </Routes>
     </HashRouter>
-    
   );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,12 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { HelmetProvider } from 'react-helmet-async';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 
 createRoot(document.getElementById('root')).render(
-  <HelmetProvider>  
     <StrictMode>
       <App />
     </StrictMode>,
-  </HelmetProvider>
 )
